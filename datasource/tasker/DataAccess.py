4336dff389892e6f8d590ec03b285ec05ebf8d22
Mattia Agostinetto <agomat@gmail.com>
2016-10-07 17:24:27 +0200

    Do send_mail

--- datasource/tasker/DataAccess.py
+++ datasource/tasker/DataAccess.py
@@ -1,8 +1,9 @@
 # This class should be the only of this package to use app/Model
 
 from django.db import IntegrityError
-from django.db.models import Min
+from django.db.models import Min, Count
 from app.models import Owner, Album, Photo
+from django.core.mail import get_connection, EmailMultiAlternatives
 
 
 class DataAccess(object):
@@ -49,4 +50,30 @@ class DataAccess(object):
             )
             photo.save()
         except IntegrityError as e:
-            print '%s Photo exists already' % (media_url)
+            print '%s Photo exists already' % media_url
+
+        # step 4 - Send mail when the counter reach precise values (Api fetching will no stop)
+
+        aggregation = Photo.objects.filter(album=album).aggregate(Count('tw_id')) # TODO test it
+        counter = aggregation.get('tw_id___count')
+        if counter in [100,200,300,400,501]:
+            DataAccess.send_mail( topic_name, counter )
+
+    @staticmethod
+    def send_mail(topic_name, counter): # fake
+        print "SEND MAIL"
+        print "%s has %d photos" % (topic_name, counter)
+        print "SEND MAIL END"
+        return True # do not disturb
+
+        msg = EmailMultiAlternatives(
+
+            "%s has %d photos" % ( topic_name, counter ),
+            "I'm awesome",
+            "E*ap Hashtag <Has*ag@E*er*pp.com>",
+            ["<ag***t@gmail.com>"],
+            "<da*de@E*er*pp.com>",
+            connection=get_connection()
+        )
+
+        msg.send()
