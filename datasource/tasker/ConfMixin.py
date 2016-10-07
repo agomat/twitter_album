dbeb70519499b6c34f06a8a54bf5cf63d700f68b
Mattia Agostinetto <agomat@gmail.com>
2016-10-07 08:10:01 +0200

    Introduce Twitter configuration Mixin

---
+++ datasource/tasker/ConfMixin.py
@@ -0,0 +1,8 @@
+class ConfMixin(object):
+
+    def __init__(self):
+        self.consumer_key = ''
+        self.consumer_secret = ''
+        self.access_token_key = ''
+        self.access_token_secret = ''
+        self.topic = '#carnival'
